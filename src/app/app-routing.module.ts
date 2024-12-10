fb354776c7085fe13012b450572b789df2bb8c56
Kranthi Kumar Solipuram <kranthikumar.solipuram@snaddevelopers.com>
2024-12-10 11:48:15 +0530

    Routing and Layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { AuthenticationLayoutComponent } from './layouts/authentication-layout/authentication-layout.component';
+import { AUTHENTICATION_ROUTES } from './routing/authetication-routing';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ADMIN_ROUTES } from './routing/admin-routing';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:AuthenticationLayoutComponent, children: AUTHENTICATION_ROUTES},
+  {path:'admin', component:AdminLayoutComponent, children: ADMIN_ROUTES},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { AutenticationLayoutComponent } from './authentiction/autentication-layout/autentication-layout.component';
+import { AuthenticationLayoutComponent } from './layouts/authentication-layout/authentication-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationLayoutComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
